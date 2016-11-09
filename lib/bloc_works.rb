1664cf6fd99525dd5f4dfa0eb1d0900b71015866
Russell Schmidt <russell.schmidt@me.com>
2016-11-08 17:45:14 -0800

    first commit

---
+++ lib/bloc_works.rb
@@ -0,0 +1,11 @@
+require "bloc_works/version"
+
+module BlocWorks
+  class Application
+  	def call(env)
+  		my_content = "<DOCTYPE html><html><head></head><body><h1 style='text-align:center;'>Hello Blocheads!</h1><div><img src='https://goo.gl/7mbPVM' alt='bully bulldog'></div></body></html>"
+
+  		[200, {'Content-Type'=>'text/html'}, [my_content]]
+  	end
+  end
+end
